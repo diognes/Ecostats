d230b9ae5b22c48e4bc2f35ca96cef21c93d923c
Dio <diognes210@gmail.com>
2022-03-21 23:38:06 -0500

    Initial commit

---
+++ R/operacion_simple.R
@@ -0,0 +1,15 @@
+#' A operation Function
+#'
+#' This function allows you to do a little operation
+#' @param x the number that you want to use 
+#' @keywords operacion
+#' @export
+#' @examples
+#' operacion_simple()
+
+
+operacion_simple<-function(x){
+              out.x<- x*5/30
+              return(out.x)
+}
+
