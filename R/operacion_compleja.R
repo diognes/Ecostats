9e064c14c0b94e8b6c0e2309695d65ee2347fc09
Dio <diognes210@gmail.com>
2022-03-23 21:46:22 -0500

    operacion compleja

---
+++ R/operacion_compleja.R
@@ -0,0 +1,16 @@
+#' A operation Function
+#'
+#' This function allows you to do a simple operation
+#' @param x the number that you want to use 
+#' @keywords operacion
+#' @export
+#' @examples
+#' operacion_simple()
+
+
+
+
+operacion_compleja<-function(x){
+  y<- x*30/20*6
+  return(y)
+}
